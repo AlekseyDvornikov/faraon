2edcefd6c94e0a294ac5e2e73b1fad1c339ac924
advornikov <advornikov@WS64-025.neoflex.ru>
2017-02-09 11:56:20 +0400

    created Thread Project

---
+++ ThreadProject/ThreadProject/src/main/java/com/fara/controller/ThreadController.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.fara.controller;
+
+/**
+ * @author advornikov
+ *
+ */
+public class ThreadController {
+
+	/**
+	 * 
+	 */
+	public ThreadController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
